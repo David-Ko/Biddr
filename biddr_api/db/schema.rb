8e6f5a64aae14390a3a77393d067b09aa4d7ff3c
David Ko <dkoflow@gmail.com>
2019-02-12 10:29:35 -0800

    Created auction and bid controllers

--- biddr_api/db/schema.rb
+++ biddr_api/db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_175130) do
+ActiveRecord::Schema.define(version: 2019_02_12_182131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auctions", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.date "ends_at"
+    t.integer "reserve_price"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_auctions_on_user_id"
+  end
+
+  create_table "bids", force: :cascade do |t|
+    t.integer "price"
+    t.bigint "auction_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["auction_id"], name: "index_bids_on_auction_id"
+    t.index ["user_id"], name: "index_bids_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -25,4 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_175130) do
     t.index ["password_digest"], name: "index_users_on_password_digest", unique: true
   end
 
+  add_foreign_key "auctions", "users"
+  add_foreign_key "bids", "auctions"
+  add_foreign_key "bids", "users"
 end
